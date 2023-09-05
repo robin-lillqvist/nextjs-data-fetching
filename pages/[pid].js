a25afe4bab4454eb1c7fa994cda7c69ce7644b23
Robin Lillqvist <robin.lillqvist@knowit.se>
2023-09-05 11:16:23 +0200

    built in failsafes

--- pages/[pid].js
+++ pages/[pid].js
@@ -5,6 +5,7 @@ import path from "path";
 function ProductDetails(props) {
   const { loadedProduct } = props;
 
+  // Fallback function in case the product is not loaded yet
   if (!loadedProduct) {
     return <p>Loading...</p>;
   }
@@ -17,16 +18,27 @@ function ProductDetails(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  // save params from url/context and save them
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  // Save params from url/context and save them
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+
   const product = data.products.find((product) => product.id === productId);
+
+  // If there is no product, send notFound to the page so it shows he 404 page instead.
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -36,15 +48,23 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      //, { params: { pid: "p2" } }, { params: { pid: "p3" } }
-    ],
+    paths: pathsWithParams,
     fallback: true,
 
     // Blocking makes fallbacks obsolete, and waits until the page loads regularly
     // fallback: "blocking",
+
+    /* Makes use of a fallback function inside the component
+    Use this if you believe you will get pageloads of nonexistent pages */
+    // fallback: true,
+
+    // fallback: false,
   };
 }
 
